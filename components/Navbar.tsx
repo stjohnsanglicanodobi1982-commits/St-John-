2a7b63015d656062c6924abe3bc40a3f563c8eb7
stjohnsanglicanodobi1982-commits <stjohnsanglicanodobi1982@gmail.com>
2026-08-15 16:11:21 +0100

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default function Navbar() {
+export function Navbar() {
   return (
     <nav style={{padding: '20px', background: '#1a3b8b', color: 'white'}}>
       <h2>St. John's Anglican Church Odobi</h2>
