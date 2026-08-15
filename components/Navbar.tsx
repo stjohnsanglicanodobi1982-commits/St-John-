292f33200e70ea0f16ec612c3154da5acf79686b
stjohnsanglicanodobi1982-commits <stjohnsanglicanodobi1982@gmail.com>
2026-08-15 13:44:35 +0100

    Add Navbar component for site navigation

---
+++ components/Navbar.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Navbar() {
+  return (
+    <nav style={{padding: '20px', background: '#1a3b8b', color: 'white'}}>
+      <h2>St. John's Anglican Church Odobi</h2>
+    </nav>
+  );
+}
